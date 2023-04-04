99ab3b5e82807349318ca86fbaf0d1024a071f47
sebastien pralong <sebastien.pralong@free.fr>
2023-04-04 14:39:34 +0100

    New Changement de l'emplacement de la balise script

--- front/js/script.js
+++ front/js/script.js
@@ -1,22 +1,25 @@
-let url ="https://localhost:3000/api/products";
+let url ="http://localhost:3000/api/products";
 
 // Récupération des pièces depuis le fichier JSON
-const reponse = fetch('../models/Product');
-const pieces = reponse.json();
+const reponse =  fetch('../../back/models/Products.js')
+// .then(products => products.json());
+const products = reponse.json();
 
+function genererProducts(products){
+    for (let i = 0; i < products.length; i++) {
 
-const article = products[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.imageUrl;
-const nomElement = document.createElement("h3");
-nomElement.innerText = article.name;
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description;
+        const article = products[0];
+        const imageElement = document.createElement("img");
+        imageElement.src = article.imageUrl;
+        const nomElement = document.createElement("h3");
+        nomElement.innerText = article.name;
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = article.description;
 
-const sectionItems = document.querySelector('#items');
-sectionItems.appendChild(imageElement);
-sectionItems.appendChild(nomElement);
-sectionItems.appendChild(descriptionElement);
+        const sectionItems = document.querySelector('#items');
+        sectionItems.appendChild(imageElement);
+        sectionItems.appendChild(nomElement);
+        sectionItems.appendChild(descriptionElement);
 
-
-
+    }
+}
