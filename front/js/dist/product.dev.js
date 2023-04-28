9593e06bcf25be3855a34557aea54392e8c43f53
sebastien pralong <sebastien.pralong@free.fr>
2023-04-28 19:02:30 +0200

    NEW 1er pas pour le local storage

--- front/js/dist/product.dev.js
+++ front/js/dist/product.dev.js
@@ -1,9 +1,7 @@
 "use strict";
 
 // Récupération de l'id dans l'Url
-var produit = window.location.search.split("?").join("");
-console.log(produit);
-var produitData = []; // Récupération des données du produit grâce à l'id
+var produit = window.location.search.split("?").join(""); // Récupération des données du produit grâce à l'id
 
 var fetchProduit = function fetchProduit() {
   fetch("http://localhost:3000/api/products/".concat(produit)).then(function (res) {
@@ -32,7 +30,70 @@ function genererProduct(promise) {
     colorsElement.textContent = promise.colors[i];
     var sectionColors = document.getElementById("colors");
     sectionColors.appendChild(colorsElement);
+  } // // comment ajouter différent produit dans le LS ?
+  //         if(localStorage.setItem("produit") != localStorage.setItem("produit"))
+  //         localStorage.setItem.push;
+  //     });
+
+}
+
+; // function savePanier(panier) {
+//     const ajoutPanier = document.getElementById("addToCart");
+//     ajoutPanier.addEventListener("click", () => {
+//         const couleur = document.getElementById("colors").value;
+//         const quantity = document.getElementById("quantity").value;
+//         const panier = {"id" : produit, "couleur" : couleur, "quantité" : quantity};
+//         localStorage.setItem("panier", JSON.stringify(panier));
+//     })
+// };
+
+var getLs = JSON.parse(localStorage.getItem("panier"));
+
+function addPanier() {
+  var couleur = document.getElementById("colors").value;
+  var quantity = document.getElementById("quantity").value;
+  var panier = {
+    id: produit,
+    couleur: couleur,
+    quantité: quantity
+  };
+  var getLs = JSON.parse(localStorage.getItem("panier"));
+  getLs.forEach(function (element, key) {
+    if (element.id === produit && element.couleur === couleur) {
+      // getLs{key}.quantité = element.quantité + quantity
+      alert("oui");
+    } else {
+      localStorage.setItem("panier", JSON.stringify(panier));
+    }
+  });
+
+  if (panier.id == panier.id) {
+    panier.quantité += quantity;
+  } else {
+    panier.push;
   }
 }
 
 ;
+var ajoutPanier = document.getElementById("addToCart");
+ajoutPanier.addEventListener("click", addPanier); // function getPanier() {
+//    let panier = localStorage.getItem("panier");
+//    if( panier == null){
+//         return [];
+//    } else {
+//     return JSON.parse(panier);
+//    }
+// };
+// function addPanier(produit) {
+//     let panier = getPanier();
+//     let foundProduit = panier.find(p => p.id == panier.id);
+//     if(foundProduit != undefined) {
+//         panier.quantity++;
+//     }else{
+//         panier.quantity =1;
+//         panier.push(produit);
+//     }
+//     savePanier(panier);
+// };
+// addPanier();
+// console.log()
--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,5 @@
 // Récupération de l'id dans l'Url
 const produit = window.location.search.split("?").join("");
-console.log(produit);
-let produitData = [];
 
 // Récupération des données du produit grâce à l'id
 const fetchProduit = () => {
@@ -26,7 +24,7 @@ function genererProduct(promise){
     const sectionItems = document.querySelector(".item__img");
     sectionItems.appendChild(imageElement);
 
-    // Boucle pour le menu déroulant des couleurs
+// Boucle pour le menu déroulant des couleurs
     for (let i = 0; i < promise.colors.length; i++) {
 
     const colorsElement = document.createElement("option");
@@ -35,4 +33,77 @@ function genererProduct(promise){
     const sectionColors = document.getElementById("colors");
     sectionColors.appendChild(colorsElement);
     }
+
+
+// // comment ajouter différent produit dans le LS ?
+//         if(localStorage.setItem("produit") != localStorage.setItem("produit"))
+//         localStorage.setItem.push;
+
+//     });
 };
+
+// function savePanier(panier) {
+//     const ajoutPanier = document.getElementById("addToCart");
+//     ajoutPanier.addEventListener("click", () => {
+//         const couleur = document.getElementById("colors").value;
+//         const quantity = document.getElementById("quantity").value;
+//         const panier = {"id" : produit, "couleur" : couleur, "quantité" : quantity};
+//         localStorage.setItem("panier", JSON.stringify(panier));
+//     })
+// };
+
+let getLs = JSON.parse(localStorage.getItem("panier"));
+
+function addPanier() {
+    const couleur = document.getElementById("colors").value;
+    const quantity = document.getElementById("quantity").value;
+    const panier = {id : produit, couleur : couleur, quantité : quantity};
+    let getLs = JSON.parse(localStorage.getItem("panier"));
+    getLs.forEach((element, key) => {
+        if(element.id === produit && element.couleur === couleur) {
+            // getLs{key}.quantité = element.quantité + quantity
+            alert("oui");
+        }else{
+            localStorage.setItem("panier", JSON.stringify(panier));
+        }
+
+    });
+    if(panier.id == panier.id) {
+        panier.quantité += quantity;
+    }else{
+        panier.push;
+    }
+};
+
+
+    const ajoutPanier = document.getElementById("addToCart");
+    ajoutPanier.addEventListener("click", (addPanier));
+
+
+
+
+// function getPanier() {
+//    let panier = localStorage.getItem("panier");
+//    if( panier == null){
+//         return [];
+//    } else {
+//     return JSON.parse(panier);
+//    }
+// };
+
+// function addPanier(produit) {
+//     let panier = getPanier();
+//     let foundProduit = panier.find(p => p.id == panier.id);
+//     if(foundProduit != undefined) {
+//         panier.quantity++;
+//     }else{
+//         panier.quantity =1;
+//         panier.push(produit);
+//     }
+//     savePanier(panier);
+// };
+
+// addPanier();
+
+
+// console.log()
